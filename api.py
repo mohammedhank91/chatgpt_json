5bb41b6a9009a530c41e2a03a10146e970b91504
mohammed chaaraoui <shoshomedo876@gmail.com>
2023-10-26 16:39:57 +0100

    update

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import openai
 import requests  
 openai.api_key = "Empty if you don't use embeddings, otherwise your hugginface token"
-openai.api_base = "https://animated-pancake-gwjg5p5rq9r29g56-1337.app.github.dev/v1"
+openai.api_base = "https://supreme-space-waffle-r9w6x7j7pxhxwqr-1337.app.github.dev/v1"
 import json , csv , pandas as pd
 
 def main():
@@ -35,13 +35,42 @@ def main():
             if content != None:
                 content_list.append(content)
                 print(content, end="", flush=True)
+        print()
+        print("".join(content_list))
         
+    num_cvs = 10  # Change this to the desired number of CVs
+
+    # Create a loop to generate and save the CVs
+    for i in range(1, num_cvs + 1):
+        # Construct the filename using string formatting
+        filename = f'cv{i}.json'
+
+        # Replace this with the content you want to save in each CV
+        content_list = ["This is CV number", str(i)]
+
+        # Save the text to the JSON file
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write("".join(content_list))
+        
+    # Load data from the JSON file
+    with open(f'cv{i}.json', 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        
+
+    # Open a CSV file for writing
+    with open('cv1.csv', 'w', newline='', encoding='utf-8') as csv_file:
+        # Create a CSV writer
+        csv_writer = csv.writer(csv_file)
+
+        # Write the header row if needed (replace fieldnames with your own)
+        fieldnames = ['name', 'adresse', 'age']  # Replace with your actual field names
+        csv_writer.writerow(fieldnames)
+
+        # Write the data to the CSV file
+        for item in data:
+            # Replace fieldnames with the corresponding keys from your JSON data
+            row = [item['field1'], item['field2'], item['field3']]  # Replace with your actual keys
+            csv_writer.writerow(row)
     
 if __name__ == "__main__":
     main()
-
-
-
-# extractedText = "Your extracted text goes here" # Replace with the actual extracted text
-# document.getElementById("message-input").value = extractedText
-# $("#message-input").val(extractedText);
