f1c3f964b7e86237e374e5a4987b40c941317ebe
mohammed chaaraoui <shoshomedo876@gmail.com>
2023-10-26 15:16:01 +0100

    update

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, render_template ,jsonify
-import PyPDF2
-import mammoth
-import xlrd
+import PyPDF2 , mammoth , xlrd
 
 app = Flask(__name__,template_folder='templates')
 
